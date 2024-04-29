1ff00a702c9d182988a524a874559704e833090a
isackmwangasu <mwangasuisack@gmail.com>
2024-04-29 09:44:35 +0300

    codedone

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+
+  const [transaction, setTransaction] = useState([])
+  const [query, setQuery] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:8001/transactions" + query)
+      .then((resp) => resp.json())
+      .then(transaction => setTransaction(transaction))
+  },
+[query])
+
+  function handlesearch(o) {
+  setQuery(o.target.value)
+  }
   return (
     <div>
-      <Search />
+      <Search handlesearch={handlesearch} />
       <AddTransactionForm />
-      <TransactionsList />
+      <TransactionsList transaction={transaction } />
     </div>
   );
 }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React,{useState} from "react";
 
 function AddTransactionForm() {
+  const [date, setDate] = useState("")
+  const [description, setDescription] = useState("")
+  const [category, setCategory] = useState("")
+  const [amount, setAmount] = useState("")
+
+  function handleSubmit(o) {
+    fetch("http://localhost:8001/transactions", {
+      method: "POST",
+      headers: { "content-Type": "application/json" },
+
+      body: JSON.stringify({
+        date: date,
+        description: description,
+        category: category,
+        amount:amount,
+      }),
+    })
+  alert("Added innit")
+  }
+  
   return (
     <div className="ui segment">
-      <form className="ui form">
+      <form onSubmit={handleSubmit}  className="ui form">
         <div className="inline fields">
-          <input type="date" name="date" />
-          <input type="text" name="description" placeholder="Description" />
-          <input type="text" name="category" placeholder="Category" />
-          <input type="number" name="amount" placeholder="Amount" step="0.01" />
+          <input value={date} onChange={(o)=>setDate(o.target.value)} type="date" name="date" />
+          <input value={description} onChange={(o)=>setDescription(o.target.value)} type="text" name="description" placeholder="Description" />
+          <input value={category} onChange={(o)=>setCategory(o.target.value)} type="text" name="category" placeholder="Category" />
+          <input value={amount} onChange={(o)=>setAmount(o.target.value)} type="number" name="amount" placeholder="Amount" step="0.01" />
         </div>
         <button className="ui button" type="submit">
           Add Transaction
